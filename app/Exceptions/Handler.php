3cb728b46a2bcec36a3b14379bd9c43bdd60afcd
SC Graphics <scgraphics@SCs-iMac.local>
2019-01-02 09:41:14 -0500

    implement query exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -82,6 +83,15 @@ class Handler extends ExceptionHandler
 
         if ($exception instanceof HttpException) {
             return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof QueryException) {
+
+            $errorCode = $exception->errorInfo[1];
+
+            if($errorCode == 1451) {
+                return $this->errorResponse('Cannot remove this resource.  It\'s related to another resource', 409);
+            }
         }
 
         return parent::render($request, $exception);

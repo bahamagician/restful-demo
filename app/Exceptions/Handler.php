d21c503f25ebc2c15c3e7eeb3e8d60283eeac54b
SC Graphics <scgraphics@SCs-iMac.local>
2019-01-02 10:00:36 -0500

    implement unexpected exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -94,7 +94,13 @@ class Handler extends ExceptionHandler
             }
         }
 
-        return parent::render($request, $exception);
+        if(config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected exception.  Try again later', 500);
+
+
     }
 
     /**

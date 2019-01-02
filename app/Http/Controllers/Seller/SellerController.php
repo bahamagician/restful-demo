b0ba87d826da444f8f1d9027e9298e8f0eaeb53f
SC Graphics <scgraphics@SCs-iMac.local>
2019-01-02 10:29:45 -0500

    implement global scope for seller implicit bindings

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -15,8 +15,7 @@ class SellerController extends ApiController
      */
     public function index()
     {
-        $sellers = Seller::has('products')->get();
-
+        $sellers = Seller::all();
         return $this->showAll($sellers);
     }
 
@@ -27,10 +26,8 @@ class SellerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Seller $seller)
     {
-        $seller = Seller::has('products')->findOrFail($id);
-
         return $this->showOne($seller);
     }
 }
